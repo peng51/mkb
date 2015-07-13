e9279994ab616d0485095af578aa265b476a929f
Yang Peng <pengyang823@gmail.com>
2015-07-13 15:43:36 -0400

    finished pattern extraction and category extraction

--- cpl.py
+++ cpl.py
@@ -8,6 +8,13 @@ instances = []
 p_patterns = ['mayor of']
 p_instances = []
 
+dt = 'DT'
+prep = 'IN'
+noun = 'NN'
+nouns = 'NNS'
+verb = 'VB'
+adj = 'JJ'
+
 def single_extract_candidates(data):
 	print "in func: singe_extract_candidates"	
 	# extract category instances
@@ -19,16 +26,124 @@ def single_extract_candidates(data):
 		get_patterns(ins)		
 
 def get_patterns(s, ins):
+	# preceding patterns empty room, optionally or non-optionally 
+	# print "preceding: [nouns] - verbs - adjectives/prepositions/determiners"
+	# print "preceding: nouns&adjectives - adjectives/prepositions/determiners"
+	# print "following: verbs - noun phrases/preposition"
 	parts = s.split(ins)
-	for part in parts:
-		print part
-		print npc.postag(part)
-		if check(part):
-			print pattern
-			# preceding: [nouns] - verbs - adjectives/prepositions/determiners
-			# preceding: nouns&adjectives - adjectives/prepositions/determiners
-			# following: verbs - noun phrases/preposition
-			p_patterns.append(pattern)			
+	for i in range(len(parts)):
+		part = parts[i]
+		#print part
+		ptag = npc.postag(part)
+		# print ptag
+		if i < len(parts) - 1:
+			p1 = ext1rule(ptag)
+			p2 = ext2rule(ptag)
+			if len(p1) != 0: p_patterns.append(p1)
+			if len(p2) != 0: p_patterns.append(p2)
+		if i > 0:	
+			p3 = ext3rule(ptag)
+			if len(p3) != 0: p_patterns.append(p3)
+	print "pattens extracted: "
+	for p in p_patterns:
+		print p
+
+def ext1rule(ptag):
+	s = []
+	x = -1
+	for i in range(len(ptag) - 1, -1, -1):
+		if 'VB' in ptag[i][1]:
+			s.append(ptag[i][0])
+			x = i
+			break 
+		elif 'DT' in ptag[i][1] or 'JJ' in ptag[i][1] or 'IN' in ptag[i][1]:
+			s.append(ptag[i][0])
+		else:
+			break
+	if x == -1: return ''
+	for i in range(x - 1, -1, -1):
+		if ptag[i][1] == 'NN' or ptag[i][1] == 'NNS':
+			s.append(ptag[i][0])
+			break
+		elif 'VB' in ptag[i][1]: 
+			s.append(ptag[i][0])
+		else:
+			break	
+	if len(s) == 0: return ''
+	else: 
+		t = ''
+		for i in range(len(s) - 1, -1, -1):
+			t += s[i] + ' '
+		return t[:len(t) - 1]
+	#x = -1
+	#for i in range(len(ptag)):
+	#	#print ptag[i][0], ptag[i][1]
+	#	if 'VB' in ptag[i][1]:
+	#		x = i
+	#		break
+	#if x == -1: return ''
+	#s = ''
+	#if x > 0 and (ptag[x - 1][1] == 'NN' or ptag[x - 1][1] == 'NNS'):
+	#		s += ptag[x - 1][0] + " "
+	#s += ptag[x][0] + " "
+	#for i in range(x + 1, len(ptag)):
+	#	if 'VB' in ptag[i][1] or 'DT' in ptag[i][1] or 'JJ' in ptag[i][1] or 'IN' in ptag[i][1]:
+	#		s += ptag[i][0] + " "
+	#	else:
+	#		#return ""
+	#		break
+	#if len(s) == 0: return s
+	#else: return s[:len(s) - 1]			 		
+
+def ext2rule(ptag):
+	s = []
+	for i in range(len(ptag) - 1, -1, -1):
+		if 'VB' in ptag[i][1]:
+			return ''
+		elif ptag[i][1] == 'NN' or ptag[i][1] == "NNS":
+			s.append(ptag[i][0])
+			break
+		elif 'DT' in ptag[i][1] or 'JJ' in ptag[i][1] or 'IN' in ptag[i][1]:
+			s.append(ptag[i][0])
+		else:
+			break
+	if len(s) == 0: return ''
+	else: 
+		t = ''
+		for i in range(len(s) - 1, -1, -1):
+			t += s[i] + ' '
+		return t[:len(t) - 1]
+	#x = -1
+	#for i in range(len(ptag)):
+	#	#print ptag[i][0], ptag[i][1]
+	#	if 'VB' in ptag[i][1]:
+	#		return ""
+	#	if ptag[i][1] == 'NN' or ptag[i][1] == "NNS":
+	#		x = i
+	#		break
+	#if x == -1: return ''
+	#s = ptag[x][0] + " "
+	#for i in range(x + 1, len(ptag)):
+	#	if 'DT' in ptag[i][1] or 'JJ' in ptag[i][1] or 'IN' in ptag[i][1]:
+	#		s += ptag[i][0] + " "
+	#	else:
+			#return ""
+	#		break
+	#if len(s) == 0: return s
+	#else: return s[:len(s) - 1]	
+	
+
+def ext3rule(ptag):
+	s = ''
+	for i in range(len(ptag)):
+		if ('VB' in ptag[i][1]):
+			s += ptag[i][0] + " "
+		elif len(s) != 0 and ptag[i][1] == 'IN':
+			s += ptag[i][0] + " "
+		else:
+			break
+	if len(s) == 0: return s
+	else: return s[:len(s) - 1]
 
 def match(s, p): # match a single pattern
 	print "in func: match string with pattern"
@@ -50,5 +165,6 @@ def match(s, p): # match a single pattern
 
 if  __name__ == "__main__":
 	print "in func: main"
+	#print npc.postag("being acquired by Google")
 	#match(data[0], p_patterns[0])
 	get_patterns(data[0], "New York")
